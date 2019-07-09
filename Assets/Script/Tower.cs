4c319430ea0bdca8a3e8234d348ecfe4e82e9e02
lzl <354244489@qq.com>
2019-07-09 16:52:23 +0800

    1

--- Assets/Script/Tower.cs
+++ Assets/Script/Tower.cs
@@ -8,6 +8,9 @@ public class Tower : MonoBehaviour {
     private GameObject attackRangNode;
     private Color beforeColor;
     public GameObject buttlePosition;
+    public GameObject buttlePrefab;
+    public float buttleDisTime = 0.2f;
+    private float buttleTimeVal = 0;
     public int towerLevel = 0;
     public Color[] levelColorst = {
         Color.blue,
@@ -32,12 +35,17 @@ public class Tower : MonoBehaviour {
                 return dis <= rang;
             });
         }
+        buttleTimeVal += Time.deltaTime;
         if (targetEnemy) {
             Vector3 temp = targetEnemy.transform.position - transform.position;
             temp.y = 0;
             Quaternion target = Quaternion.LookRotation (temp);
             transform.rotation = Quaternion.Slerp (transform.rotation, target, Time.deltaTime * 5); //插值的缓慢旋转
             // transform.rotation = target;//很僵硬的旋转
+            if (buttleTimeVal >= buttleDisTime) {
+                addButtle ();
+                buttleTimeVal = 0;
+            }
         }
     }
     public double getRang () {
@@ -63,13 +71,23 @@ public class Tower : MonoBehaviour {
         if (!attackRangNode) {
             attackRangNode = Instantiate (attackRangPrefab);
             attackRangNode.transform.parent = transform;
-            float D = (1.1f - transform.position.y) / transform.localScale.y;//1.1刚好比路高0.1
+            float D = (1.1f - transform.position.y) / transform.localScale.y; //1.1刚好比路高0.1
             attackRangNode.transform.localPosition = new Vector3 (0, D, 0);
         }
-        Debug.Log (attackRangNode.transform.position.y);
         attackRangNode.SetActive (true);
         float rangF = (float) getRang () * 1.28f * 2;
         attackRangNode.transform.localScale = new Vector3 (rangF, rangF, rangF);
     }
-    
+    void addButtle () {
+        GameObject buttleOne = Global.GetButtlePool.GetObject ();
+        if (!buttleOne) {
+            buttleOne = Instantiate (buttlePrefab);
+            Global.GetButtlePool.PushObject (buttleOne);
+            buttleOne.transform.parent = transform.parent;
+        }
+        buttleOne.transform.position = buttlePosition.transform.position;
+        buttleOne.SetActive (true);
+        buttleOne.GetComponent<buttle>().addTarget(targetEnemy);
+    }
+
 }
---
+++ Assets/Script/buttle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttle : MonoBehaviour {
+    private GameObject targetEnemy;
+    public float speed = 0.3f;
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!targetEnemy) {
+            Debug.LogError ("没有目标");
+        }
+        if(!targetEnemy.activeInHierarchy){
+            Global.GetButtlePool.PushObject (gameObject);
+            // Debug.LogError ("目标超出界面");
+        }
+        transform.position  = Vector3.MoveTowards (transform.position, targetEnemy.transform.position, speed);
+        float dis = Vector3.Distance (transform.position, targetEnemy.transform.position);
+        if (dis < 0.1f) {
+            onTouchEnemy ();
+        }
+    }
+    public void addTarget (GameObject obj) {
+        targetEnemy = obj;
+    }
+    void onTouchEnemy () {
+        Global.GetButtlePool.PushObject (gameObject);
+    }
+}
